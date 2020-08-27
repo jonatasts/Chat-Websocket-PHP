30f68ecfe624b3793e70ad7e88e6cf42bde1112f
Jonatas Santos <jonatas.santos@produs.dom>
2020-08-26 21:47:31 -0300

    Implementada barra de pesquisa por contato

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,27 @@ $(document).ready(function () {
         $('.action_menu').toggle();
     });
 });
+
+//TABLE ->Write on keyup event of keyword input element
+$("#search").keyup(function () {
+	if ($("#search").val() != "") {
+		_this = this;
+		// Show only matching TR, hide rest of them
+		$.each($("ul.contacts li"), function () {
+			if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
+				$(this).hide();
+			else
+				$(this).show();
+		});
+	} else {
+		let cont = 1;
+
+		$.each($("ul.contacts li"), function () {
+			if (cont <= 7)
+				$(this).show();
+			else
+				$(this).hide();
+			cont++;
+		});
+	}
+});
