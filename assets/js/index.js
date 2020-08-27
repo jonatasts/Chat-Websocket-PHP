de7f432aadc9bfe1488b25de836f10b1199d90a2
Jonatas Santos <jonatas.santos@produs.dom>
2020-08-26 22:21:31 -0300

    Implementada a troca da classe active nas li's dos contatos

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,24 +6,34 @@ $(document).ready(function () {
 
 //TABLE ->Write on keyup event of keyword input element
 $("#search").keyup(function () {
-	if ($("#search").val() != "") {
-		_this = this;
-		// Show only matching TR, hide rest of them
-		$.each($("ul.contacts li"), function () {
-			if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
-				$(this).hide();
-			else
-				$(this).show();
-		});
-	} else {
-		let cont = 1;
+    if ($("#search").val() != "") {
+        _this = this;
+        // Show only matching TR, hide rest of them
+        $.each($("ul.contacts li"), function () {
+            if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
+                $(this).hide();
+            else
+                $(this).show();
+        });
+    } else {
+        let cont = 1;
 
-		$.each($("ul.contacts li"), function () {
-			if (cont <= 7)
-				$(this).show();
-			else
-				$(this).hide();
-			cont++;
-		});
-	}
+        $.each($("ul.contacts li"), function () {
+            if (cont <= 7)
+                $(this).show();
+            else
+                $(this).hide();
+            cont++;
+        });
+    }
+});
+
+$(".contacts li").click(function () {
+    if (!$(this).hasClass("active")) {
+        $.each($("ul.contacts li.active"), function () {
+            $(this).removeClass("active");
+        });
+
+        $(this).addClass("active");
+    }
 });
